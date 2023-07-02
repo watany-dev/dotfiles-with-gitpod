97fc3027c43c6aa8145494dad20259ee15318cc3
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-02 19:59:23 +0900

    Update setting_tools.sh

--- scripts/setting_tools.sh
+++ scripts/setting_tools.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Serrubf Gitleaks
 sudo cp /home/gitpod/.dotfiles/bin/gitleaks /usr/local/bin/
 sudo chmod +x /usr/local/bin/gitleaks 
+
+# Setting husky
+npx husky-init && npm install
+echo '#!/usr/bin/env sh
+. "$(dirname -- "$0")/_/husky.sh"
+
+gitleaks protect --staged' > .husky/pre-commit
+chmod +x .husky/pre-commit 
