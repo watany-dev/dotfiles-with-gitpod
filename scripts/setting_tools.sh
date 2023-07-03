d782fd89a7c04bfed607bb94ee7fcd6627803e5b
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-03 10:35:18 +0900

    Update setting_tools.sh

--- scripts/setting_tools.sh
+++ scripts/setting_tools.sh
@@ -5,6 +5,9 @@ sudo cp /home/gitpod/.dotfiles/bin/gitleaks /usr/local/bin/
 sudo chmod +x /usr/local/bin/gitleaks 
 
 # Setting husky
+repo_root=$(git rev-parse --show-toplevel)
+cd "$repo_root"
+
 npx husky-init && npm install
 echo '#!/usr/bin/env sh
 . "$(dirname -- "$0")/_/husky.sh"
