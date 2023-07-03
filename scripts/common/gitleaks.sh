7203348d48ea93a271acff8ea05a69b0b4b4a4bd
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-03 01:55:48 +0000

    husky

--- scripts/common/gitleaks.sh
+++ scripts/common/gitleaks.sh
@@ -3,14 +3,3 @@
 # Setup Gitleaks
 sudo cp /home/gitpod/.dotfiles/bin/gitleaks /usr/local/bin/
 sudo chmod +x /usr/local/bin/gitleaks 
-
-# Setting husky
-repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root"
-
-npx husky-init && npm install
-echo '#!/usr/bin/env sh
-. "$(dirname -- "$0")/_/husky.sh"
-
-gitleaks protect --staged' > .husky/pre-commit
-chmod +x .husky/pre-commit 
--- setup.sh
+++ setup.sh
@@ -6,10 +6,14 @@ CURRENT=$PWD
 
 cd $TMPDIR
 
-for script in ~/.dotfiles/scripts/*; do
+for script in ~/.dotfiles/scripts/common/*; do
   bash "$script"
 done
 
 cd $CURRENT
 
 rm -rf $TMPDIR
+
+for script in ~/.dotfiles/scripts/precommit/*; do
+  bash "$script"
+done
