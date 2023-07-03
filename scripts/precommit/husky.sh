0b34268b051abbc0f6f91058026a0e3d680bb139
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-03 11:06:27 +0900

    Update husky.sh

--- scripts/precommit/husky.sh
+++ scripts/precommit/husky.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Setting husky
-repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root"
+# move work dirctory
+subdir=$(find /workspaces -maxdepth 1 -mindepth 1 -type d | head -n 1)
+if [ -d "$subdir" ]; then
+    cd "$subdir"
+    echo "Moved to: $(pwd)"
+else
+    echo "No subdirectory found in /workspaces"
+fi
 
+# Setting husky
 npx husky-init && npm install
 echo '#!/usr/bin/env sh
 . "$(dirname -- "$0")/_/husky.sh"
