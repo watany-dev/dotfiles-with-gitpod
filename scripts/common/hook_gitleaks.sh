d4156975742c0c8aa5c2b2617d5500413af9d1c1
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-04 11:08:04 +0900

    Create hook_gitleaks.sh

---
+++ scripts/common/hook_gitleaks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# move work dirctory
+TARGET_DIR='/workspace'
+subdir=$(find "$TARGET_DIR" -maxdepth 1 -mindepth 1 -type d ! -name ".*" | head -n 1)
+if [ -d "$subdir" ]; then
+    cd "$subdir"
+    echo "Moved to: $(pwd)"
+else
+    echo "No subdirectory found in $TARGET_DIR"
+fi
+
+# Setting pre-commit
+pip install --upgrade pip
+pip install pre-commit
+echo 'repos:
+  - repo: https://github.com/gitleaks/gitleaks
+    rev: v8.17.0
+    hooks:
+      - id: gitleaks' > .pre-commit-config.yaml
+pre-commit autoupdate
+pre-commit install
