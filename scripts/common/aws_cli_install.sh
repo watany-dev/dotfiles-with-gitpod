907cf0a1be6d0bc5d247ae18c411bd58afead8a0
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-14 10:02:41 +0900

    Update aws_cli_install.sh

--- scripts/common/aws_cli_install.sh
+++ scripts/common/aws_cli_install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# if command -v aws &> /dev/null; then
-#     exit
-# fi
-
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-
 # Check AWS CLI version
 if ! command -v aws &> /dev/null || aws --version 2>&1 | grep -q 'aws-cli/1.'; then
     echo "Installing AWS CLI v2..."
 
-    # AWS CLI v2のインストール
+    # Install AWS CLI v2
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
@@ -22,7 +14,7 @@ else
     echo "AWS CLI v2 is already installed."
 fi
 
-# AWS CLI bash-completionの設定
+# AWS CLI bash-completion
 echo "Setting up AWS CLI bash completion..."
 if [ -f /usr/local/bin/aws_completer ]; then
     echo "complete -C '/usr/local/bin/aws_completer' aws" >> ~/.bashrc
